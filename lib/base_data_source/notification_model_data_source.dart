0fcd194ca4b38e998328dcc74dab83ba33b794fa
JacobOdd <antonpichka@gmail.com>
2021-12-22 22:44:53 +0200

    change file notification_model_data_source. Specifical add jeneric Y and change method cancelNotification parameter jeneric T on the Y

--- lib/base_data_source/notification_model_data_source.dart
+++ lib/base_data_source/notification_model_data_source.dart
@@ -2,8 +2,8 @@
 import 'package:library_architecture_mvvm_modify/base_exception/local_exception.dart';
 import 'package:library_architecture_mvvm_modify/base_response/response.dart';
 
-abstract class NotificationModelDataSource<T> {
+abstract class NotificationModelDataSource<T,Y> {
   Future<Response<bool,LocalException>> pushNotificationToZonedSchedule(T model);
   Future<Response<bool,LocalException>> pushNotificationToShow(T model);
-  Future<Response<bool,LocalException>> cancelNotification(T model);
+  Future<Response<bool,LocalException>> cancelNotification(Y type);
 }
