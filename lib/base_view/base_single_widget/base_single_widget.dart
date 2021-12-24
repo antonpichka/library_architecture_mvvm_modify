dbdfcc9a9a905b11b2c2ec189a218b8e1f8c5c73
JacobOdd <antonpichka@gmail.com>
2021-12-24 12:14:02 +0200

    Update file BaseSingleWidget specificly add jeneric T

--- lib/base_view/base_single_widget/base_single_widget.dart
+++ lib/base_view/base_single_widget/base_single_widget.dart
@@ -2,14 +2,14 @@
 import 'package:flutter/material.dart';
 import 'package:library_architecture_mvvm_modify/base_model/domain_model.dart';
 
-abstract class BaseSingleWidget
+abstract class BaseSingleWidget<T extends DomainModel>
 {
   Widget buildSingleWidgetUsingAList(
       BuildContext context,
-      List<DomainModel> list);
+      List<T> list);
 
 
   Widget buildSingleWidgetUsingTheModel(
       BuildContext context,
-      DomainModel model);
+      T model);
 }
