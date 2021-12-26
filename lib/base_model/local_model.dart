53f7ca14d714005460119f6c34013dea54bb521b
JacobOdd <antonpichka@gmail.com>
2021-12-27 01:12:33 +0200

    update file local_model.dart delete method fromMap

--- lib/base_model/local_model.dart
+++ lib/base_model/local_model.dart
@@ -1,8 +1,6 @@
 abstract class LocalModel {
   String localUniqueId = "";
 
-  LocalModel fromMap(Map<String, dynamic> map);
-
   Map<String, dynamic> toMap();
 
   set setLocalUniqueId(String uniqueId)  {
