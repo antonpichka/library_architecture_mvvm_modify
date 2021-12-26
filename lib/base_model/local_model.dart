1da3d1e60c30bda543e0a4afac8853f5a9619272
JacobOdd <antonpichka@gmail.com>
2021-12-26 17:45:56 +0200

    update file local_model.dart

--- lib/base_model/local_model.dart
+++ lib/base_model/local_model.dart
@@ -1,7 +1,11 @@
-class LocalModel {
+abstract class LocalModel {
   String localUniqueId = "";
 
+  LocalModel fromMap(Map<String, dynamic> map);
+
+  Map<String, dynamic> toMap();
+
   set setLocalUniqueId(String uniqueId)  {
-    this.localUniqueId = uniqueId;
+    localUniqueId = uniqueId;
   }
 }
