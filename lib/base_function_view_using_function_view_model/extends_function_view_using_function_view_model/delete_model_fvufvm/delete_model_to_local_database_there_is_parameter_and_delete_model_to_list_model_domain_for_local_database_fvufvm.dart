68856a51794e54b7ef9591001d745a2bedb47562
JacobOdd <antonpichka@gmail.com>
2022-01-18 12:27:22 +0200

    delete package 'function_view' and refactory delete_.. classes

--- lib/base_function_view_using_function_view_model/extends_function_view_using_function_view_model/delete_model_fvufvm/delete_model_to_local_database_there_is_parameter_and_delete_model_to_list_model_domain_for_local_database_fvufvm.dart
+++ lib/base_function_view_using_function_view_model/extends_function_view_using_function_view_model/delete_model_fvufvm/delete_model_to_local_database_there_is_parameter_and_delete_model_to_list_model_domain_for_local_database_fvufvm.dart
@@ -6,7 +6,6 @@ import 'package:library_architecture_mvvm_modify/base_list_model/base_list_model
 import 'package:library_architecture_mvvm_modify/base_model/base_model_domain.dart';
 import 'package:library_architecture_mvvm_modify/base_view_model/base_view_model.dart';
 import 'package:library_architecture_mvvm_modify/base_view_model/enum_base_model_domain_object_operation_view_model.dart';
-import 'package:library_architecture_mvvm_modify/function_view/there_is_parameter_for_methods_type_future_function_view.dart';
 import 'package:library_architecture_mvvm_modify/function_view_model/function_view_model.dart';
 import 'package:library_architecture_mvvm_modify/function_view_model/interface_function_view_model/delete_model_fvm/delete_model_to_local_database_there_is_parameter_and_delete_model_to_list_model_domain_for_local_database_fvm.dart';
 
@@ -25,18 +24,6 @@ class DeleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainF
   }
 
   @override
-  Future<void> mainMethodAndNotifyStream(DeleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainForLocalDatabaseFVM mainMethod, BaseViewModel<BaseModelDomain, BaseListModelDomain<BaseModelDomain>> baseViewModel, Function functionForResultSuccess, Function(DomainException p1) functionForResultDomainException, Function(LocalException p1) functionForResultLocalException, Function(NetworkException p1) functionForResultNetworkException) async {
-    await FunctionViewModel.deleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainForLocalDatabaseFVM(
-        mainMethod,
-        functionForResultSuccess,
-        functionForResultDomainException,
-        functionForResultLocalException,
-        functionForResultNetworkException
-    );
-    notifyStream(baseViewModel);
-  }
-
-  @override
   void notifyStream(BaseViewModel<BaseModelDomain, BaseListModelDomain<BaseModelDomain>> baseViewModel) {
     baseViewModel.notifyStreamModelDomain(EnumBaseModelDomainObjectOperationViewModel.deleteModelToLocalDatabaseThereIsParameter);
   }
@@ -47,30 +34,4 @@ class DeleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainF
         object,
         EnumBaseModelDomainObjectOperationViewModel.deleteModelToLocalDatabaseThereIsParameter);
   }
-
-  @override
-  Future<void> setObjectAndMainMethod(DeleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainForLocalDatabaseFVM mainMethod, BaseViewModel<BaseModelDomain, BaseListModelDomain<BaseModelDomain>> baseViewModel, BaseModelDomain object, Function functionForResultSuccess, Function(DomainException p1) functionForResultDomainException, Function(LocalException p1) functionForResultLocalException, Function(NetworkException p1) functionForResultNetworkException) async {
-    setObject(baseViewModel, object);
-    await FunctionViewModel.deleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainForLocalDatabaseFVM(
-        mainMethod,
-        functionForResultSuccess,
-        functionForResultDomainException,
-        functionForResultLocalException,
-        functionForResultNetworkException
-    );
-  }
-
-  @override
-  Future<void> setObjectAndMainMethodAndNotifyStream(DeleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainForLocalDatabaseFVM mainMethod, BaseViewModel<BaseModelDomain, BaseListModelDomain<BaseModelDomain>> baseViewModel, BaseModelDomain object, Function functionForResultSuccess, Function(DomainException p1) functionForResultDomainException, Function(LocalException p1) functionForResultLocalException, Function(NetworkException p1) functionForResultNetworkException) async {
-    setObject(baseViewModel, object);
-    await FunctionViewModel.deleteModelToLocalDatabaseThereIsParameterAndDeleteModelToListModelDomainForLocalDatabaseFVM(
-        mainMethod,
-        functionForResultSuccess,
-        functionForResultDomainException,
-        functionForResultLocalException,
-        functionForResultNetworkException
-    );
-    notifyStream(baseViewModel);
-  }
-
 }
