5e32ec4b138d76f16bebdeda8da520cd99cbf2bd
JacobOdd <antonpichka@gmail.com>
2022-01-18 16:48:36 +0200

    add file 'response_generic_bool_and_domain_exception.dart'

---
+++ lib/response_generic_bool_and_domain_exception.dart
@@ -0,0 +1,8 @@
+import 'package:library_architecture_mvvm_modify/base_exception/domain_exception.dart';
+import 'package:library_architecture_mvvm_modify/response.dart';
+
+class ResponseGenericBoolAndDomainException extends Response<bool,DomainException>
+{
+  ResponseGenericBoolAndDomainException.success(bool success) : super.success(success);
+  ResponseGenericBoolAndDomainException.exception(DomainException exception) : super.exception(exception);
+}
