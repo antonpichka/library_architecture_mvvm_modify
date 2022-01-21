be448224222a30e598319d31afac2948b1851f9d
JacobOdd <antonpichka@gmail.com>
2022-01-21 19:53:57 +0200

    global update '25'

---
+++ lib/base_view/view_model_methods_for_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:library_architecture_mvvm_modify/base_model/base_list_model/base_list_model_domain.dart';
+import 'package:library_architecture_mvvm_modify/base_model/base_model_domain.dart';
+import 'package:library_architecture_mvvm_modify/base_view_model/base_view_model.dart';
+
+abstract class ViewModelMethodsForView<T extends BaseViewModel<BaseModelDomain,BaseListModelDomain<BaseModelDomain>>> {
+  @protected
+  final T viewModel;
+
+  ViewModelMethodsForView(this.viewModel);
+
+}
