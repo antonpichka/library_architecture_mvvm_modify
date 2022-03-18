e8e15edf2b93fa6a8c125177e84f9647426a800f
JacobOdd <antonpichka@gmail.com>
2022-03-18 14:10:58 +0200

    global update '61'

--- lib/base_list_of_view_models_using_realized_methods_for_named_view/base_list_of_view_models_using_realized_methods_for_named_view.dart
+++ lib/base_list_of_view_models_using_realized_methods_for_named_view/base_list_of_view_models_using_realized_methods_for_named_view.dart
@@ -27,6 +27,7 @@ abstract class BaseListOfViewModelsUsingRealizedMethodsForNamedView<T extends En
         }
       }
     });
+    print("OPA4A4A");
     setupMap.clear();
     setupMap = null;
   }
