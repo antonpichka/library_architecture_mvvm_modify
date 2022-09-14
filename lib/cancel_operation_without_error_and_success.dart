fbab81ed627df547227c41217c7dfdb9aae62be3
JacobOdd <antonpichka@gmail.com>
2022-09-14 08:31:55 +0300

    alpha_test_0.5.4

--- lib/cancel_operation_without_error_and_success.dart
+++ lib/cancel_operation_without_error_and_success.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 
 class CancelOperationWithoutErrorAndSuccess {
-  CancelOperationWithoutErrorAndSuccess(Object thisClass) {
+  CancelOperationWithoutErrorAndSuccess(Object thisClass,String message) {
     if(kDebugMode) {
       print("===start_to_trace_cancel_operation_without_error_and_success===\n");
-      print("Debug name is cancel_operation_without_error_and_success:\n(ThisClass: ${thisClass.runtimeType})");
+      print("Debug name is cancel_operation_without_error_and_success:\n(ThisClass: ${thisClass.runtimeType})\n(Message: $message)");
       print("\n===end_to_trace_cancel_operation_without_error_and_success===");
     }
   }
