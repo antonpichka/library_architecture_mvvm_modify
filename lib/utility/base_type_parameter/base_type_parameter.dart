ed8a5e836f12de075a9a58732c40dd907bf99fd4
JacobOdd <antonpichka@gmail.com>
2022-11-30 00:58:09 +0200

    alpha_test_1.2.8

--- lib/utility/base_type_parameter/base_type_parameter.dart
+++ lib/utility/base_type_parameter/base_type_parameter.dart
@@ -15,6 +15,7 @@
  */
 
 import 'package:library_architecture_mvvm_modify/utility/base_exception/base_exception.dart';
+import 'package:library_architecture_mvvm_modify/utility/base_exception/local_exception.dart';
 import 'package:library_architecture_mvvm_modify/utility/exception_controller.dart';
 
 abstract class BaseTypeParameter<T extends Object>
@@ -25,6 +26,12 @@ abstract class BaseTypeParameter<T extends Object>
   BaseTypeParameter.success(this.parameter)
       : exceptionController = ExceptionController.success();
   BaseTypeParameter.exception(BaseException exception)
+      : exceptionController = ExceptionController.exception(exception),
+        parameter = null;
+  BaseTypeParameter.successForFBDS()
+      : exceptionController = ExceptionController.success(),
+        parameter = null;
+  BaseTypeParameter.exceptionForFBDS(LocalException exception)
       : exceptionController = ExceptionController.exception(exception),
         parameter = null;
 }
