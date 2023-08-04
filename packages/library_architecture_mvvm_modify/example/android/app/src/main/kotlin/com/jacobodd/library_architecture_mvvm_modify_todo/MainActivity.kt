65bcd3676b7c4c55a7dd26eb0fcb9d62a8b68d3d
antonpichka <antonpichka@gmail.com>
2023-08-04 21:46:46 +0300

    chore(packages\library_architecture_mvvm_modify\example): update example

--- packages/library_architecture_mvvm_modify/example/android/app/src/main/kotlin/com/jacobodd/library_architecture_mvvm_modify_todo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.jacobodd.library_architecture_mvvm_modify_todo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/library_architecture_mvvm_modify/example/lib/main.dart
+++ packages/library_architecture_mvvm_modify/example/lib/main.dart
@@ -1,9 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:library_architecture_mvvm_modify_todo/model_q_named_service_view_model/named_service/sqflite_service.dart';
-import 'package:library_architecture_mvvm_modify_todo/named_view/app_view.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SqfliteService().getDatabase;
-  runApp(AppView());
+void main() {
+  print("Example");
+  print("https://github.com/antonpichka/gamelogger");
 }
